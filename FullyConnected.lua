a34f0389b0edebe04e7930df8842a595918d2fe4
Hugh Perkins <hughperkins@gmail.com>
2015-06-22 10:39:30 +0800

    improve doc at headers of FullyConnected class

--- FullyConnected.lua
+++ FullyConnected.lua
@@ -1,13 +1,21 @@
--- Use like this:
+-- FullyConnected layer
+-- ====================
 --
---    model:add(FullyConnected(15)
+--    model:add(nn.FullyConnected(numNeurons))
 --
--- ... where 15 is number of neurons (can be any positive integer, within the bounds of
--- available memory and other resources)
+-- where:
+--   numNeurons: number of neurons (can be any positive integer,
+--               within the bounds of available memory and other
+--               resources)
 --
--- this takes a 4d tensor input(batchSize, inPlanes, inSize, inSize)
+-- input:
+--   4d tensor input(batchSize, inPlanes, inSize, inSize)
 --
--- ... and converts it to 4d tensor output(batchSize, numNeurons, 1, 1)
+-- output:
+--   4d tensor output(batchSize, numNeurons, 1, 1)
+--
+-- FullyConnected wraps a Linear layer inside two Reshape layers,
+-- see the updateOutput method below for more details
 
 local FullyConnected, parent = torch.class('nn.FullyConnected', 'nn.Sequential')
 
@@ -21,28 +29,20 @@ function FullyConnected:updateOutput(input)
     if( input:size():size() ~= 4) then
       error("input should be 4d tensor")
     end
-    print('FC:update output type(input)', torch.type(input))
     local batchSize = input:size(1)
     local inPlanes = input:size(2)
     local inH = input:size(3)
     local inW = input:size(4)
     local r1 = nn.Reshape(batchSize, inPlanes * inH * inW)
     r1:type(torch.type(input))
---    r1._input = input.__constructor()
---    r1._gradOutput = input.__constructor()
-    print('type r1._input', torch.type(r1._input))
     self:add(r1)
     local linear = nn.Linear(inPlanes * inH * inW, self.neurons)
     linear:type(torch.type(input))
-    print('type linear.output', torch.type(linear.output))
     self:add(linear)
     self.gradWeight = linear.gradWeight
     self.gradBias = linear.gradBias
     local r2 = nn.Reshape(batchSize, self.neurons, 1, 1)
     r2:type(torch.type(input))
---    r2._input = input.__constructor()
---    r2._gradOutput = input.__constructor()
-    print('type r2._input', torch.type(r2._input))
     self:add(r2)
   end
   parent.updateOutput(self, input)
