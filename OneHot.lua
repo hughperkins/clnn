6798cdf849cf862d9dda30febf806e8794c548dd
Hugh Perkins <hughperkins@gmail.com>
2015-06-25 07:23:29 +0800

    add OneHot.lua file from char-cnn repo (not tested or anything yet...

---
+++ OneHot.lua
@@ -0,0 +1,23 @@
+-- this is copied from https://github.com/karpathy/char-rnn/blob/master/util/OneHot.lua
+-- for now, so I can test if it works on clnn
+
+local OneHot, parent = torch.class('OneHot', 'nn.Module')
+
+function OneHot:__init(outputSize)
+  parent.__init(self)
+  self.outputSize = outputSize
+  -- We'll construct one-hot encodings by using the index method to
+  -- reshuffle the rows of an identity matrix. To avoid recreating
+  -- it every iteration we'll cache it.
+  self._eye = torch.eye(outputSize)
+end
+
+function OneHot:updateOutput(input)
+  self.output:resize(input:size(1), self.outputSize):zero()
+  if self._eye == nil then self._eye = torch.eye(self.outputSize) end
+  self._eye = self._eye:float()
+  local longInput = input:long()
+  self.output:copy(self._eye:index(1, longInput))
+  return self.output
+end
+
