cc1c6b90e30e2e8723a5e921b651a316ef4fc81f
Hugh Perkins <hughperkins@gmail.com>
2015-07-12 17:26:06 +0800

    remove some hd5500 cl compile warnings

--- Threshold.lua
+++ Threshold.lua
@@ -1,15 +1,25 @@
 require 'nn'
 require 'cltorch'
 
+local function floatToString(val)
+  valstring = tostring(val)
+  if valstring:find('%.') or valstring:find('e') then
+    valstring = valstring .. 'f'
+  end
+  return valstring
+end
+
 function torch.ClTensor.nn.Threshold_updateOutput(self, input)
 --  print('torch.nn.ReLU
 --  self.output:map(input, "*out = *in1 > 0.0f ? *in1 : 0.0f")
+  self.thresholdstring = floatToString(self.threshold)
+  self.valstring = floatToString(self.val)
   if self.inplace then
-    input:apply("*out = (*out > " .. self.threshold .. ") ? *out : " .. self.val)
+    input:apply("*out = (*out > " .. self.thresholdstring .. ") ? *out : " .. self.valstring)
     self.output = input
   else
     self.output:resize(input:size())
-    self.output:map(input, "*out = ( *in1 > " .. self.threshold .. ") ? *in1 : " .. self.val)
+    self.output:map(input, "*out = ( *in1 > " .. self.thresholdstring .. ") ? *in1 : " .. self.valstring)
   end
   return self.output
 end
@@ -21,10 +31,10 @@ function torch.ClTensor.nn.Threshold_updateGradInput(self, input, gradOutput)
      self.gradInput:zero()
   end
   if self.inplace then
-    gradOutput:map2(input, gradOutput, "*out = (*in1 > " .. self.threshold .. ") ? *in2 : 0")
+    gradOutput:map2(input, gradOutput, "*out = (*in1 > " .. self.thresholdstring .. ") ? *in2 : 0.0f")
     self.gradInput = gradOutput
   else
-    self.gradInput:map2(input, gradOutput, "*out = (*in1 > " .. self.threshold .. ") ? *in2 : 0")
+    self.gradInput:map2(input, gradOutput, "*out = (*in1 > " .. self.thresholdstring .. ") ? *in2 : 0.0f")
   end
 --  self.gradInput:map2(gradOutput, self.output, "*out = *in2 > 0.0f ? *in1 : 0.0f")
   return self.gradInput
