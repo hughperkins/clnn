b068949cfab0bad2d8c66d8a14ea5b8a6789d3b8
Hugh Perkins <hughperkins@gmail.com>
2015-08-11 21:20:28 +0800

    reindent Statefultimer

--- StatefulTimer.lua
+++ StatefulTimer.lua
@@ -1,31 +1,31 @@
 local StatefulTimer = torch.class('nn.StatefulTimer')
 
 function StatefulTimer:__init(prefix)
-  self.times = {}
-  self.counts = {}
-  if prefix == nil then
-    prefix = ''
-  end
-  self.prefix = prefix
-  self.timer = torch.Timer()
-  self.last = self.timer:time().real
+   self.times = {}
+   self.counts = {}
+   if prefix == nil then
+      prefix = ''
+   end
+   self.prefix = prefix
+   self.timer = torch.Timer()
+   self.last = self.timer:time().real
 end
 
 function StatefulTimer:state(name)
-  if self.times[name] == nil then
-    self.times[name] = 0
-    self.counts[name] = 0
-  end
-  now = self.timer:time().real
-  change = now - self.last
-  self.last = now
-  self.times[name] = self.times[name] + change
-  self.counts[name] = self.counts[name] + 1
+   if self.times[name] == nil then
+      self.times[name] = 0
+      self.counts[name] = 0
+   end
+   now = self.timer:time().real
+   change = now - self.last
+   self.last = now
+   self.times[name] = self.times[name] + change
+   self.counts[name] = self.counts[name] + 1
 end
 
 function StatefulTimer:dump()
-  for k,v in pairs(self.times) do
-    print(self.prefix .. k, v, self.counts[k])
-  end
+   for k,v in pairs(self.times) do
+      print(self.prefix .. k, v, self.counts[k])
+   end
 end
 
