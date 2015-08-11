f3febbe09f6f7258e8ee4bf5325bae3d032d3985
Hugh Perkins <hughperkins@gmail.com>
2015-08-11 21:11:48 +0800

    reindent Narrow.lua

--- Narrow.lua
+++ Narrow.lua
@@ -17,20 +17,20 @@ nn.Narrow.baseUpdateGradInput = nn.Narrow.updateGradInput
 
 
 function nn.Narrow:updateGradInput(input, gradOutput)
-  if torch.type(input) ~= 'torch.ClTensor' then
-    return self:baseUpdateGradInput(input, gradOutput)
-  end
-  if self._doneinit == nil then
---    print('resizing gradinput')
-    self.gradInput:resizeAs(input)
-    self.gradInput:zero();
-    self._doneinit = true
-  end
-
---  self.gradInput:resizeAs(input)  
---  self.gradInput:zero();
-  self.gradInput:narrow(self.dimension,self.index,self.length):copy(gradOutput)
-
-  return self.gradInput
+   if torch.type(input) ~= 'torch.ClTensor' then
+      return self:baseUpdateGradInput(input, gradOutput)
+   end
+   if self._doneinit == nil then
+      --    print('resizing gradinput')
+      self.gradInput:resizeAs(input)
+      self.gradInput:zero();
+      self._doneinit = true
+   end
+   
+   --  self.gradInput:resizeAs(input)
+   --  self.gradInput:zero();
+   self.gradInput:narrow(self.dimension,self.index,self.length):copy(gradOutput)
+   
+   return self.gradInput
 end
 
