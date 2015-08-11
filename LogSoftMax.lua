743acb3ec73d86dcd924164ba6c1644b719f6c0e
Hugh Perkins <hughperkins@gmail.com>
2015-08-11 21:09:55 +0800

    convert logsoftmax

--- LogSoftMax.lua
+++ LogSoftMax.lua
@@ -1,50 +1,49 @@
 function torch.ClTensor.nn.LogSoftMax_updateOutput(self, input)
---  cltorch.setTrace(1)
-  if   input:dim() ~= 2 then
-    error('LogSoftMax expects 2-d tensor currently')
-  end
-  if self.maxbuffer == nil then
---    self.buffer = input:clone()
-    self.maxbuffer, self.resind = input:max(2)
-    self.vec_size = input:size(2)
-  end
-  self.output:resize(input:size())
-
-if true then
-if true then
-  self.maxbuffer:max(self.resind, input, 2)
-
-  self.output:copy(input)
-  self.output:csub(self.maxbuffer:expand(input:size(1), input:size(2)))
-  self.output:exp()
-  self.maxbuffer:sum(self.output,2)
-
-  self.output:cdiv(self.maxbuffer:expand(input:size(1), input:size(2)))
-  self.output:log()
-end
-end
---  cltorch.setTrace(0)
-  return self.output
+   --  cltorch.setTrace(1)
+   if   input:dim() ~= 2 then
+      error('LogSoftMax expects 2-d tensor currently')
+   end
+   if self.maxbuffer == nil then
+      --    self.buffer = input:clone()
+      self.maxbuffer, self.resind = input:max(2)
+      self.vec_size = input:size(2)
+   end
+   self.output:resize(input:size())
+   
+   if true then
+      if true then
+         self.maxbuffer:max(self.resind, input, 2)
+         
+         self.output:copy(input)
+         self.output:csub(self.maxbuffer:expand(input:size(1), input:size(2)))
+         self.output:exp()
+         self.maxbuffer:sum(self.output,2)
+         
+         self.output:cdiv(self.maxbuffer:expand(input:size(1), input:size(2)))
+         self.output:log()
+      end
+   end
+   --  cltorch.setTrace(0)
+   return self.output
 end
 
 function torch.ClTensor.nn.LogSoftMax_updateGradInput(self, input, gradOutput)
-  local nElement = self.gradInput:nElement()
-  self.gradInput:resizeAs(input)
-  if self.gradInput:nElement() ~= nElement then
-     self.gradInput:zero()
-  end
-
---  cltorch.setTrace(1)
-  if true then
-  self.maxbuffer:sum(gradOutput, 2)
-  self.gradInput:copy(self.output)
-  self.gradInput:exp()
-  self.gradInput:cmul(self.maxbuffer:expand(input:size(1), input:size(2)))
-  self.gradInput:neg()
-  self.gradInput:add(gradOutput)
+   local nElement = self.gradInput:nElement()
+   self.gradInput:resizeAs(input)
+   if self.gradInput:nElement() ~= nElement then
+      self.gradInput:zero()
+   end
+   
+   --  cltorch.setTrace(1)
+   if true then
+      self.maxbuffer:sum(gradOutput, 2)
+      self.gradInput:copy(self.output)
+      self.gradInput:exp()
+      self.gradInput:cmul(self.maxbuffer:expand(input:size(1), input:size(2)))
+      self.gradInput:neg()
+      self.gradInput:add(gradOutput)
+   end
+   --  cltorch.setTrace(0)
+   return self.gradInput
 end
---  cltorch.setTrace(0)
-  return self.gradInput
-end
-
 
