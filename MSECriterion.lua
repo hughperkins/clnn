310540827e17f482e49e39861c7d0437b5619284
Hugh Perkins <hughperkins@gmail.com>
2015-09-22 08:57:58 +0800

    tidy up global variables into local variables in MSECriterion.lua

--- MSECriterion.lua
+++ MSECriterion.lua
@@ -3,12 +3,12 @@ function torch.ClTensor.nn.MSECriterion_updateOutput(self, input, target)
       self.workBuffer = input:clone()
    end
    
-   se = 0   
    self.workBuffer:copy(input)
    self.workBuffer:csub(target)
    self.workBuffer:pow(2)
-   se = torch.sum(self.workBuffer)
+   local se = torch.sum(self.workBuffer)
    
+   local mse = se
    if self.sizeAverage then
       mse = se / torch.numel(input)
    end
@@ -16,9 +16,9 @@ function torch.ClTensor.nn.MSECriterion_updateOutput(self, input, target)
 end
 
 function torch.ClTensor.nn.MSECriterion_updateGradInput(self, input, target)
-   norm = 2
+   local norm = 2
    if self.sizeAverage then
-      size = torch.numel(input)
+      local size = torch.numel(input)
       norm = norm / size
    end
    self.gradInput:resize(target:size())
