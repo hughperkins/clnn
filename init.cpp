f51258fea2d209b8d0c9b731401f03337d7c1927
Hugh Perkins <hughperkins@gmail.com>
2015-12-29 22:51:34 +0800

    added clnn.about()

--- init.cpp
+++ init.cpp
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <iostream>
+#include <stdexcept>
+using namespace std;
+
 extern "C" {
   #include "lua.h"
+//  #include "lauxlib.h"
+  #include "utils.h"
+//  #include "luaT.h"
 }
-#include <iostream>
-using namespace std;
+
+#include "commit_generated.h"
 
 extern "C" {
     int luaopen_libclnn( lua_State *L );
@@ -15,15 +22,35 @@ void clnn_SpatialMaxPooling_init(lua_State *L);
 void clnn_SpatialAveragePooling_init(lua_State *L);
 void clnn_SoftMax_init(lua_State *L);
 
+static int clnn_about(lua_State *L)
+{
+  cout << "clnn.  OpenCL backend for Torch nn" << endl;
+  cout << "Built from commit " << commit << endl;
+  cout << "More info, doc: https://github.com/hughperkins/clnn" << endl;
+  cout << "Issues: https://github.com/hughperkins/clnn/issues" << endl;
+  return 0;
+}
+
+static const struct luaL_Reg clnn_stuff__ [] = {
+  {"about", clnn_about},
+  {NULL, NULL}
+};
+
 int luaopen_libclnn( lua_State *L ) {
-  lua_newtable(L);
-//    printf("luaopen_libclnn called :-)\n");
+  try {
+    lua_newtable(L);
+    luaL_setfuncs(L, clnn_stuff__, 0);
+
+  //    printf("luaopen_libclnn called :-)\n");
     clnn_ELU_init(L);
     clnn_SpatialConvolutionMM_init(L);
     clnn_SpatialMaxPooling_init(L);
     clnn_SpatialAveragePooling_init(L);
     clnn_SoftMax_init(L);
 //    cout << " try cout" << endl;
-    return 1;
+  } catch(runtime_error &e) {
+    THError("Something went wrong: %s", e.what());
+  }
+  return 1;
 }
 
--- utils.h
+++ utils.h
@@ -8,7 +8,16 @@ extern "C" {
 #endif
 
 #include <lua.h>
+#include "luaT.h"
+#include "TH.h"
 struct THClState* getCltorchState(lua_State* L);
+
+#if LUA_VERSION_NUM == 501
+/*
+** Adapted from Lua 5.2.0
+*/
+void luaL_setfuncs (lua_State *L, const luaL_Reg *l, int nup);
+#endif
 
 #ifdef __cplusplus 
 } // extern "C"
