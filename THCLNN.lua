b2a81ed935b28140cfb2ac39e8cb28d1a48f5e5a
Hugh Perkins <hughperkins@gmail.com>
2016-01-04 21:29:44 +0800

    remove errors about AbsCriterion at runtime

--- THCLNN.lua
+++ THCLNN.lua
@@ -22,19 +22,6 @@ TH_API void THNN_ClAbs_updateGradInput(
           THClTensor *input,
           THClTensor *gradOutput,
           THClTensor *gradInput);
-
-TH_API void THNN_ClAbsCriterion_updateOutput(
-          THClState *state,
-          THClTensor *input,
-          THClTensor *target,
-          float *output,
-          bool sizeAverage);
-TH_API void THNN_ClAbsCriterion_updateGradInput(
-          THClState *state,
-          THClTensor *input,
-          THClTensor *target,
-          THClTensor *gradInput,
-          bool sizeAverage);
 ]]
 
 local preprocessed = string.gsub(THCLNN_h, 'TH_API ', '')
